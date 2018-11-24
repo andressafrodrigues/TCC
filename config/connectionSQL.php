0b748ecbd39df182839723110ad5ebd522a13656
Andressa Rodrigues <andressaf.rodrigues@hotmail.com>
2018-11-24 17:49:22 -0200

    Login inacabado

---
+++ config/connectionSQL.php
@@ -0,0 +1,16 @@
+<?php  
+$servername = "localhost";
+$username = "root";
+$password = null;
+$database = "db_tcc";
+$conn = mysqli_connect($servername, $username, $password,$database) or die("Erro na conexão");
+
+if (!mysqli_set_charset($conn, "utf8")) {
+    printf("Error loading character set utf8: %s\n", mysqli_error($conn));
+    exit();
+} else {
+    printf("Current character set: %s\n", mysqli_character_set_name($conn));
+}
+
+
+?>
